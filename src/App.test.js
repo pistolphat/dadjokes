3ec0b597b18c1c8e35cb43836644096c39a78203
Giraficode <giraficode@gmail.com>
2021-10-15 18:32:08 +0000

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// @ponicode
+describe("handleClick", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.handleClick({ preventDefault: () => false })
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            inst.handleClick({ preventDefault: () => true })
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction = () => {
+            inst.handleClick(undefined)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("componentDidMount", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.componentDidMount()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
